04e914f3fe16e4ba0bda6b1940d92bb7a8a86264
LaureanoVera <laureanoigvera@gmail.com>
2021-04-22 14:37:27 -0300

    slider con javascipt en showcase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// ====== Scroll Reveal ======
 document.querySelector('.menu-responsive').addEventListener('click', () => {
   document.querySelector('.nav-main__menu').classList.toggle('show');
 });
@@ -10,3 +11,29 @@ ScrollReveal().reveal('.banner-one', { delay: 300 });
 ScrollReveal().reveal('.banner-two', { delay: 300 });
 ScrollReveal().reveal('.banner-three', { delay: 300 });
 
+// ====== Cambio de Fondo Showcase ======
+const thisImage = document.querySelector('.showcase');
+let countImage = 1;
+thisImage.style.backgroundImage=`url('../images/showcase-0${countImage}.jpg')`;
+
+  // Left Changes
+const leftChange = () => {
+  if (countImage > 1) {
+    countImage -= 1;
+  }
+  return thisImage.style.backgroundImage=`url('../images/showcase-0${countImage}.jpg')`;
+}
+
+const buttonLeft = document.querySelector('.showcase__left');
+buttonLeft.addEventListener('click', leftChange);
+
+  // Right Changes
+const rightChange = () => {
+  if (countImage < 4) {
+    countImage += 1;
+  }
+  return thisImage.style.backgroundImage=`url('../images/showcase-0${countImage}.jpg')`;
+}
+
+const buttonRight = document.querySelector('.showcase__right');
+buttonRight.addEventListener('click', rightChange);
