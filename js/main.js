3d00b3b21d79cf2ffbbd5c25da45f60d460ca202
LaureanoVera <laureanoigvera@gmail.com>
2021-04-22 14:57:11 -0300

    barra de scroll añadida

--- js/main.js
+++ js/main.js
@@ -37,3 +37,17 @@ const rightChange = () => {
 
 const buttonRight = document.querySelector('.showcase__right');
 buttonRight.addEventListener('click', rightChange);
+
+// ====== Barra de Scroll ======
+window.addEventListener('scroll', () => {
+  const top = document.documentElement.scrollTop;
+
+  const heightObj = {
+    scroll: document.documentElement.scrollHeight,
+    client: document.documentElement.clientHeight
+  }
+  const height = heightObj.scroll - heightObj.client;
+
+  const totalScrolled = (top / height) * 100;
+  document.querySelector('.scroll-bar').style.width = `${totalScrolled}%`;
+});
